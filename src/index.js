9670e432cf20d7edaed6af06eb05df483cb9d75d
daxoliveira <diogenes.oliveira@rxpro.com.br>
2021-07-16 13:09:38 -0300

    implement http methods

--- src/index.js
+++ src/index.js
@@ -6,6 +6,26 @@ app.get("/", (request, response) => {
   return response.json({
     message: "Hello World Rocketseat Ignite"
   })
-})
+});
+
+app.get("/courses", (request, response) => {
+  return response.json(["Course 1", "Course 2", "Course 3"]);
+});
+
+app.post("/courses/:id", (request, response) => {
+  return response.json(["Course 1", "Course 2", "Course 3", "Course 4"]);
+});
+
+app.put("/courses/:id", (request, response) => {
+  return response.json(["Course 6", "Course 2", "Course 3", "Course 4"]);
+});
+
+app.patch("/courses/:id", (request, response) => {
+  return response.json(["Course 6", "Course 7", "Course 3", "Course 4"]);
+});
+
+app.delete("/courses/:id", (request, response) => {
+  return response.json(["Course 6", "Course 2", "Course 4"]);
+});
 
 app.listen(3333);
